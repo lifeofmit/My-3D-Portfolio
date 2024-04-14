93f8d7b16a7b68eb253ffe6cf951526916180ad4
Mit Joshi <mitjoshi0207@gmail.com>
2024-04-15 01:41:27 +0530

    added spin animation in react logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import { NavLink } from "react-router-dom";
 import { logo } from "../assets/images";
 
 const Navbar = () => {
+  const spinnerStyle = {
+    animation: 'spin 5s linear infinite'
+};
   return (
     <header className='header'>
       <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+        <img style={spinnerStyle} src={logo} alt='logo' className='w-18 h-18 object-contain' />
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
